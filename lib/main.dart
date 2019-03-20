ff568f91e42f66c3801e913e954d453775ed08b6
rohit645 <kumarnath.rohit@gmail.com>
2019-03-20 09:51:05 +0530

    added theme data

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,18 @@ class MyApp extends StatefulWidget {
     return _MyAppState();
   }
 }
-
+ 
 class _MyAppState extends State<MyApp> {
   
-
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.blueGrey, 
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text('Easy List'),
