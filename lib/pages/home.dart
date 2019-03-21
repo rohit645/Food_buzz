4592339c8f9fa2f55c7cb0dca2320d6d412bd9e9
rohit645 <kumarnath.rohit@gmail.com>
2019-03-21 18:57:04 +0530

    added back button, used maps instead of Strings

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import '../productmanager.dart';
 
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(
-            title: Text('Easy List'),
-          ),
-          body: ProductManager(), // now making   named arguments
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('Easy List'),
+      ),
+      body: ProductManager(), // now making   named arguments
     );
   }
 }
